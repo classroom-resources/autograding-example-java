3e6e16261828522ce63f8c105e77895671287107
Nathaniel Woodthorpe <d12@github.com>
2020-02-26 16:11:02 -0500

    Update TestHello.java

--- src/test/java/com/example/project/TestHello.java
+++ src/test/java/com/example/project/TestHello.java
@@ -22,7 +22,7 @@ public class TestHello {
      Hello.main(null);
 
      // assertion
-     assertEquals("Hello World!\n", bos.toString());
+     assertEquals("Hello!\n", bos.toString());
 
      // undo the binding in System
      System.setOut(originalOut);
