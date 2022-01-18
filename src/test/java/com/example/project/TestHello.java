85b3b68191a321e0f4f18465a0a071ae08775378
maltebenjamin <46089659+maltebenjamin@users.noreply.github.com>
2022-01-18 19:17:55 +0100

    Updated gradle autograding for pass on Windows
    Changed line seperator to an universial one, due to issues with `gradle test` on local windows machines.

--- src/test/java/com/example/project/TestHello.java
+++ src/test/java/com/example/project/TestHello.java
@@ -22,7 +22,7 @@ public class TestHello {
      Hello.main(null);
 
      // assertion
-     assertEquals("Hello world!\n", bos.toString());
+     assertEquals("Hello world!" + System.lineSeparator(), bos.toString());
 
      // undo the binding in System
      System.setOut(originalOut);
