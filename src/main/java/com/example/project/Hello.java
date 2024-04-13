e688ba6f767e5923c827af9f70228e6d518471c4
briancasio <113497684+briancasio@users.noreply.github.com>
2024-04-13 00:39:37 -0600

    Update Hello.java

--- src/main/java/com/example/project/Hello.java
+++ src/main/java/com/example/project/Hello.java
@@ -3,7 +3,7 @@ package com.example.project;
 public class Hello {
 
 	public static void main(final String[] args) {
-		System.out.println("Not the right string, tests will fail!");
+		System.out.println("Hello World!\n");
 	}
 
 }
